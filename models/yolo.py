6bbe14b367f712a7b94b4fc109c557e5f4bc3a8d
Jaskirat Singh <juskirat2000@gmail.com>
2021-06-03 17:19:51 +0530

    Update yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -74,7 +74,7 @@ class Detect(nn.Module):
 
 
 class Model(nn.Module):
-    def __init__(self, cfg='yolov3.yaml', ch=3, nc=None, anchors=None):  # model, input channels, number of classes
+    def __init__(self, cfg='yolov3-tiny.yaml', ch=3, nc=None, anchors=None):  # model, input channels, number of classes
         super(Model, self).__init__()
         if isinstance(cfg, dict):
             self.yaml = cfg  # model dict
